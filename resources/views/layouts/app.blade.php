e6bf71835cbfc8e4623a7a6bd022a6e3d6374be6
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-08 16:51:54 +0100

    Commented

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,11 +55,12 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
-                        @if (Route::has('register'))
+                        {{-- // Commento queste due Route per non avere altri utenti registrati. --}}
+                        {{-- @if (Route::has('register'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
-                        @endif
+                        @endif --}}
                         @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
