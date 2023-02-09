ba27a65f48e241c1cc47eeb5cbd0a43bd945f12f
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-09 12:08:15 +0100

    Added Logged Content

--- resources/views/pages/logged.blade.php
+++ resources/views/pages/logged.blade.php
@@ -5,6 +5,19 @@
 
 @section('content')
     <div class="container">
-        <h1 class="m-5">Public</h1>
+        <h1 class="m-5">Logged</h1>
+        <h1 class="m-5">Projects</h1>
+        <ul>
+            @foreach ($projects as $project)
+                <li>
+                    Name: {{ $project -> name }}<br>
+                    Description: {{ $project -> description }}<br>
+                    Image: {{ $project -> main_image }}<br>
+                    Release: {{ $project -> release_date }}<br>
+                    Repo-Link: {{ $project -> repo_link }}<br>
+                    <br>
+                </li>
+            @endforeach
+        </ul>
     </div>
 @endsection
