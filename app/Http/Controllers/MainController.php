49d49c7bffe6000d4d8fdb006a54dc0cd0b3ac30
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-09 11:56:53 +0100

    Imported Model and new home function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Importo il model "Project"
+use App\Models\Project;
+
 class MainController extends Controller
 {
     // Home Pubblica
+    // public function home()
+    // {
+    //     return view('pages.home');
+    // }
+
     public function home()
     {
-        return view('pages.home');
+        $projects = Project::all();
+
+        return view('pages.home', compact('projects'));
     }
 
     // Logged Privato
