2491e753479484dcfef20ba5228d301e534787c3
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-09 11:56:18 +0100

    Update public home content

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,5 +3,22 @@
 @section('content')
     <div class="container">
         <h1 class="m-5">Public</h1>
+        <h1 class="m-5">Projects</h1>
+        <ul>
+            @foreach ($projects as $project)
+                <li>
+                    Name: {{ $project -> name }}<br>
+                    Description: {{ $project -> description }}<br>
+                    Image: {{ $project -> main_image }}<br>
+                    Release: {{ $project -> release_date }}<br>
+                    Repo-Link: {{ $project -> repo_link }}<br>
+                    <br>
+                </li>
+            @endforeach
+        </ul>
     </div>
 @endsection
+
+
+
+    
